58332f0767b01beadfd16177edcf8115bc07235c
zlsheepcity <zl.sheepcity@gmail.com>
2018-05-13 16:30:14 +0300

    quick start

--- run.js
+++ run.js
@@ -2,7 +2,7 @@ console.time('timer__start_C');
 var cc, ccc;
 cc = function(log) { console.log(log); };
 ccc = function(log) { 
-    console.group('R:'+log[log.length-1]);
+    console.group('o:'+log[log.length-1]);
     for ( row in log ) cc(log[row]);
     console.groupEnd();
 };
@@ -11,82 +11,139 @@ cc('timer__start_C: ~0');
 
 // ---------------------------------------------- DATA
 
-var city;
-city = {
+var City;
+City = {
     name:'index',
     xoxox:'xoxox',
-    //goods:[],
-    map:'true'
+    goods:[],
+    map:true
 };
 
-var resources;
-resources = [
+var Resources;
+Resources = [
     {
         name:'content',
+        lvl:1,
         map:'true'
+    },
+    {
+        name:'html_tag',
+        lvl:0,
+        map:true
+    }
+];
+
+var Landmarks;
+Landmarks = [
+    {
+        name:'voronsnest',
+        lvl:0,
+        map:true
     }
 ];
 
 // ---------------------------------------------- UNIVERSE
 
-var world;
-world = {
+var World;
+World = {
+    age:0,
     Welcome:
-    function() {
-        cc('world created');
-    }
+        function() {
+            this.age++;
+            cc('# world created');
+        }
 };
 
-var planeta; // prince: world.display;
-planeta = {
+var Planeta; // prince: world.display
+Planeta = {
+    age:0,
     Welcome:
-    function() {
-        cc('its show time');
-        this.city();
-        this.resources();
-    }
+        function() {
+            this.age++;
+            cc('# ---------------------- Its Show Time #'+this.age);
+            this.city();
+            this.resources();
+            this.landmarks();
+        }
+    ,
+    skyface:
+        function() {
+            if ( this.age < 1 ) 
+                cc('Display skyface: rejected, too young planet');
+            else
+                cc('Display skyface');
+        }
     ,
     city: 
-    function() {
-        if (city.map)
-        ccc([
-            city.name,
-            'DO_display.city'
-        ]);
-    }
+        function() {
+            if (City.map)
+            ccc([
+                'Name:'+City.name,
+                'Resources:',City.goods,
+                'DO_display.city'
+            ]);
+        }
     ,
     resources:
-    function() {
-        for ( i in resources )
-        if (resources[i].map)
-        ccc([
-            resources[i].name,
-            'DO_display.resource.R['+i+']'
-        ]);
-    }    
+        function() {
+            for ( i in Resources )
+                if (Resources[i].map)
+                    ccc([
+                        'name:'+Resources[i].name,
+                        'lvl:'+Resources[i].lvl,
+                        'DO_display.resources.R['+i+']'
+                    ]);
+        } 
+    ,
+    landmarks:
+        function() {
+            for ( i in Landmarks )
+                if (Landmarks[i].map)
+                    ccc([
+                        'name:'+Landmarks[i].name,
+                        'lvl:'+Landmarks[i].lvl,
+                        'DO_display.landmarks.L['+i+']'
+                    ]);
+        } 
 };
 
-var industry;
-industry = {
+var Industry; // prince: world.update
+Industry = {
+    age:0,
     Welcome:
-    function() {
-        cc('industry works');
-        var industry = this;
-        industry.supportUI();
-    }
+        function() {
+            this.age++;
+            cc('# ---------------------- Industry works #'+this.age);
+            this.gatherResources();
+            this.supportUI();
+        }
+    ,
+    gatherResources:
+        function() {
+            var fresh_goods = [];
+            for ( i in Resources ) {
+                if ( Resources[i].lvl > 0 )
+                    fresh_goods.push({ 
+                        name:Resources[i].name,
+                        id:i 
+                    });
+            }
+            ccc([fresh_goods,'DO_resources.gather']);
+            City.goods = fresh_goods;
+        }
     ,
     supportUI:
-    function() {
-        cc('Display user interface');
-    }
+        function() {
+            Planeta.skyface();         
+        }
 };
 
 
 // ---------------------------------------------- BEGIN
 
-world.Welcome();
-planeta.Welcome();
-industry.Welcome();
+World.Welcome();
+Industry.Welcome();
+Planeta.Welcome();
 
 // ---------------------------------------------- END
 console.timeEnd('timer__start_C');
