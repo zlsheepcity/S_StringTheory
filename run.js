aa0a7681b5301c90b47aae091ad740ee142ebe34
Zinovij Lipinski <zinovi@egamings.com>
2018-05-14 20:56:06 +0300

    grab resource

--- run.js
+++ run.js
@@ -42,6 +42,10 @@ function WorldResource(dna) {
     this.name = dna.name ? dna.name : 'content';
     this.lvl = dna.lvl ? dna.lvl : 0;
     this.map = dna.map ? true : false ;
+    this.Grab = function() {
+        if( this.lvl < 1 ) this.lvl = 1;
+        if( !this.map ) this.map = true;
+    };
 }
 function WorldLandmark(dna) {
     this.name = dna.name ? dna.name : 'valley';
@@ -120,7 +124,8 @@ function PrinceIndustry(world) {
     };
     this.ContactAll = function() {
         var world = this.world;
-        Story.Call('seobot1',world);
+        for ( var contact in world.contacts )
+            Story.Call( contact, world );
     };
     this.make_injection = function(patient,cure) {
         var id, i, single_action_key, single_action_value;
@@ -246,11 +251,21 @@ Story = {
     Call: function(contact, world) {
         cc('Calling '+contact+'...');
         switch (contact) {
+
             case 'seobot1':
+
                 // trade for content
                 if ( world.city.hasResource('content') )
                     cc('-- content for seobot');
                 break;
+
+            case 'admin':
+
+                // html quiz
+                if ( world.city.hasResource('html_tag') )
+                    cc('-- html for admin');
+                break;
+
         }
     },
     Tasks: {
@@ -263,262 +278,6 @@ Story = {
     }
 };
 
-/*
-var City;
-City = {
-    name:'index',
-    age:0,
-    xoxox:'xoxox',
-    goods:[],
-    on_breakfast:['GatherResources'],
-    on_dinner:['TradeOffers'],
-    on_party:['CityWelcome'],
-    center:{
-        market: {
-            name:'rooter',
-            lvl:1,
-            traders:['seobot1']
-        }
-    },
-    Welcome: function(){ cc('City welcome!') },    
-    map:true
-};
-*/
-/*
-var Resources;
-Resources = [
-    {
-        name:'content',
-        lvl:1,
-        map:'true'
-    },
-    {
-        name:'html_tag',
-        lvl:0,
-        map:true
-    }
-];
-
-var Landmarks;
-Landmarks = [
-    {
-        name:'voronsnest',
-        lvl:0,
-        map:true
-    }
-];
-*/
-
-// ---------------------------------------------- UNIVERSE
-
-/*
-var World;
-World = {
-    age:0,
-    Welcome:
-        function() {
-            this.age++;
-            cc('# world created');
-            Planeta.Welcome();
-            Industry.Welcome();
-        }
-    ,
-    GoodNight: // end turn
-        function() {
-            //
-        }
-    ,
-    Have_a_Nice_Day: // Breakfast/Dinner/Party
-        function() {
-            City.age++;
-            cc('# ---------------------- Have a Nice Day, '+City.name+' #'+City.age);
-            for ( var i in City.on_breakfast )     Industry.DoJob(City.on_breakfast[i]);
-            for ( var i in City.on_dinner )        Industry.DoJob(City.on_dinner[i]);
-            for ( var i in City.on_party )         Industry.DoJob(City.on_party[i]);
-        }
-};
-*/
-/*
-var Planeta; // prince: world.display
-Planeta = {
-    age:0,
-    Welcome:
-        function() {
-            this.age++;
-            cc('# ---------------------- Its Show Time #'+this.age);
-            this.city();
-            this.resources();
-            this.landmarks();
-        }
-    ,
-    skyface:
-        function() {
-            if ( this.age < 1 ) 
-                cc('Display skyface: rejected, too young planet');
-            else
-                cc('Display skyface');
-        }
-    ,
-    city: 
-        function() {
-            if (City.map)
-            ccc([
-                'Name:'+City.name,
-                'Resources:',City.goods,
-                'Buildings:',Industry.get_list_of_buldings(),
-                'DO_display.city'
-            ]);
-        }
-    ,
-    resources:
-        function() {
-            for ( i in Resources )
-                if (Resources[i].map)
-                    ccc([
-                        'name:'+Resources[i].name,
-                        'lvl:'+Resources[i].lvl,
-                        'DO_display.resources.R['+i+']'
-                    ]);
-        } 
-    ,
-    landmarks:
-        function() {
-            for ( i in Landmarks )
-                if (Landmarks[i].map)
-                    ccc([
-                        'name:'+Landmarks[i].name,
-                        'lvl:'+Landmarks[i].lvl,
-                        'DO_display.landmarks.L['+i+']'
-                    ]);
-        } 
-};
-*/
-
-/*
-var Industry; // prince: world.update
-Industry = {
-    age:0,
-    DoJob:
-        function(job) {
-            var Industry = this;
-            switch(job) {
-                case 'CityWelcome':
-                    City.Welcome();
-                    break;
-                case 'GatherResources':
-                    Industry.gatherResources();
-                    break;
-                case 'TradeOffers':
-                    Industry.tradeOffers();
-                    break;
-                default:
-                    cc('-- I cant do '+job);
-            }
-        }
-    ,
-    gatherResources:
-        function() {
-            var fresh_goods = [];
-            for ( i in Resources ) {
-                if ( Resources[i].lvl > 0 )
-                    fresh_goods.push({ 
-                        name:Resources[i].name,
-                        id:i 
-                    });
-            }
-            ccc([fresh_goods,'DO_resources.gather']);
-            City.goods = fresh_goods;
-        }
-    ,
-    tradeOffers:
-        function() {
-            var traders = City.center.market.traders;
-            ccc([traders, 'DO_contacts.trade']);
-            for ( i in traders )
-                cc('-- Trade offer with '+traders[i]);
-            
-        }
-    ,
-    get_list_of_buldings:
-        function() {
-            var list = [];
-            for ( i in City.center )
-                list.push(i);
-            return list;
-        }
-    ,
-    Welcome:
-        function() {
-            cc('# industry works');
-        }
-};
-*/
-
-/*
-var Contact;
-Contact = {
-    Player: {
-        WhatNext:
-            function() {
-                //if ( Contact.Autoplayer.active ) Contact.Autoplayer.WhatNext();
-                cc('player...');
-                cc(Story.chapter);
-            }
-        ,
-        active:true
-    },
-    Autoplayer: {
-        WhatNext:
-            function() {
-                cc('autoplayer...');
-                Story.NextChapter();
-            }
-        ,
-        active:true
-    },
-    DoCall:
-        function(contact) {
-            return contact.active;
-        }
-};
-*/
-
-/*
-var Story;
-Story = {
-    chapter:0,
-    Begin:
-        function() {
-            cc('# ---------------------- Read the Story #'+Story.chapter);
-            World.Welcome();
-            //Contact.Player.WhatNext();
-            Story.NextChapter();
-        }
-    ,
-    NextChapter:
-        function() {
-            Story.chapter++;
-            Story.Play();
-        }
-    ,
-    Play:
-        function() {
-            var id = Story.chapter;
-            switch(id) {
-                case 1:
-                    ccc(['---------------------- Story message '+id,'DO_story.message.C'+id]);
-                    World.Have_a_Nice_Day();
-                    Contact.Player.WhatNext();
-                    break;
-                case 0:
-                default:
-                    Story.Begin();
-            }
-        }
-};
-*/
-
-
 // ---------------------------------------------- BEGIN
 
 var World = new KingWorld();
@@ -528,24 +287,10 @@ World.Welcome();
 Planet.show();
 
 World.NextTurn();
+World.resources['html_tag'].Grab();
 World.NextTurn();
 
 // ---------------------------------------------- END
 console.timeEnd('timer__start_C');
 
 cc(World);
-
-//World.Welcome();
-//Industry.Welcome();
-//Planeta.Welcome();
-//City.center.market.Welcome();
-//World.Have_a_Nice_Day(City);
-//Story.Begin();
-/*
-var zz = new KingWorld();
-var zc = zz.city;
-cc(zz);
-cc(zc);
-for ( i in zz.resources ) cc(zz.resources[i]);
-*/
-
