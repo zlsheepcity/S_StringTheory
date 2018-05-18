235339b5a9bc2e5d82db7c6b6fcb9b06c61980e1
zlsheepcity <zl.sheepcity@gmail.com>
2018-05-18 16:09:52 +0300

    first svg

--- app/js/Galactica.js
+++ app/js/Galactica.js
@@ -4,15 +4,28 @@ var Galactica;
 Galactica = {
     resources: [
         { name:'content' },
-        { name:'html_tag' }
+        { name:'html_tag' },
+        { name:'css_rule' },
     ],
     landmarks: [
-        { name:'voronsnest' }
+        { name:'index_valley' },
+        { name:'content_fields' },
+        { name:'router_lakes' },
+        { name:'body_coast' },
+        { name:'head_peak' },
+        { name:'north_land' },
+        { name:'header_mountains' },
+        { name:'seobot_desert' },
+        { name:'sty_land' },
+        { name:'cascadia_land' },
+        { name:'sheet_peak' },
+        { name:'bold_mountains' },
+        { name:'git_river' },
     ],
     contacts: [
-        { name:'admin', lvl:1, homeland:'voronsnest' },
+        { name:'admin', lvl:1, homeland:'router_lakes' },
         { name:'scout' },
-        { name:'seobot1', lvl:1 }
+        { name:'seobot1', lvl:1 },
     ],
     chromosome:
     {
@@ -25,7 +38,9 @@ Galactica = {
             html_tag:{map:true},
         },
         landmarks:{
-            voronsnest:{map:true}
+            index_valley:{map:true}
+            content_fields:{map:true}
+            router_lakes:{map:true}
         },
         turn: 0,
         name: 'Galactica.KingWorld'
